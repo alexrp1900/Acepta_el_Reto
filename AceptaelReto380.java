1c6b44c94e9da03e4991fa580ed5dd808cb7c24f
alexrp1900 <56952231+alexrp1900@users.noreply.github.com>
2020-11-11 09:41:07 +0100

    Add files via upload

---
+++ AceptaelReto380.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AceptaelReto380 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int suma = sc.nextInt();
+		
+		int total = sc.nextInt();
+		
+		while() {
+			
+		}
+	}
+
+}
